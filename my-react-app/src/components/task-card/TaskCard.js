e5725b0ab1c54e240f36e97ceba0e7b8ffd473f3
Andrei <105882288+AndreiMihaiR@users.noreply.github.com>
2022-08-09 23:40:06 +0300

    sincronized with homework

---
+++ my-react-app/src/components/task-card/TaskCard.js
@@ -0,0 +1,21 @@
+// import "./TaskCard.css";
+// import Badge from "../badge/Badge"
+// import DateContainer from "../date-container/DateContainer"
+
+// function taskCard (props) {
+       
+//     return (
+//     <div className="card-wrapper">
+//         <div className="card-header">
+//             <p className="task-id">{props.id}</p>
+//             <Badge status1 ={props.status}/>
+//         </div>
+//         <div className="card-content">
+//             <p>{props.name}</p>
+//         </div>
+//         <div className="card-footer">
+//             <DateContainer date={props.dueDate}/>
+//         </div>
+//     </div>)
+// }
+// export default taskCard;
--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
 import "./App.css";
-import TaskCard from "./components/task-card/TaskCard";
+import TaskViewer from "./components/task-viewer/TaskViewer";
+
 function App() {
-  
   const data = [
     {
-      id: "T-2",
-      status: "Todo",
+      id: "T-1",
       name: "Create a Design System for Enum Workspace.",
-      dueDate: new Date(2022, 7, 7),
+      status: "Todo",
+      dueDate: new Date(2022, 5, 23),
+    },
+    {
+      id: "T-2",
+      name: "12 Create a Design System for Enum Workspace.",
+      status: "In Progress",
+      dueDate: new Date(2022, 7, 24),
     },
     {
       id: "T-3",
-      status: "In progres",
-      name: "Create a Design System for Enum Workspace.",
-      dueDate: new Date(2022, 7, 7),
+      name: "13 Create a Design System for Enum Workspace.",
+      status: "Complete",
+      dueDate: new Date(2022, 6, 13),
     },
     {
       id: "T-4",
-      status: "Done",
       name: "Create a Design System for Enum Workspace.",
-      dueDate: new Date(2022, 7, 7),
-    }
-  ]
+      status: "Todo",
+      dueDate: new Date(2022, 5, 23),
+    },
+  ];
 
-  return <div className="app-container">
-    <TaskCard
-    id= {data[0].id}
-    status= {data[0].status}
-    name= {data[0].name}
-    dueDate= {data[0].dueDate}
-    />
-    <TaskCard
-    id= {data[1].id}
-    status= {data[1].status}
-    name= {data[1].name}
-    dueDate= {data[1].dueDate}
-    />
-    <TaskCard
-    id= {data[2].id}
-    status= {data[2].status}
-    name= {data[2].name}
-    dueDate= {data[2].dueDate}
-    />
-  </div>;
+  return (
+    <div className="app-container">
+      <div className="app-content">
+        <h3>Task Manager</h3>
+        <TaskViewer taskList={data} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
